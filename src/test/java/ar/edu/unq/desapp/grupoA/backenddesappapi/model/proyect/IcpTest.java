d089fd43e69e0332b11924428b8b23c2c13793e4
Seba Ink <sebastian@gestioncervecera.com>
2020-09-05 19:33:11 -0300

    add Icp empty model + test

---
+++ src/test/java/ar/edu/unq/desapp/grupoA/backenddesappapi/model/proyect/IcpTest.java
@@ -0,0 +1,17 @@
+package ar.edu.unq.desapp.grupoA.backenddesappapi.model.proyect;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class IcpTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+}
