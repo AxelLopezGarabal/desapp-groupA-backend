59f0afeeec7afd43c974416ec805f3554a0b36f5
AxelLopezGarabal <axel.lopez.garabal@gmail.com>
2020-09-10 17:24:30 -0300

    fixed issue >> Unused import

--- src/test/java/ar/edu/unq/desapp/grupoa/backenddesappapi/model/proyect/ProjectTest.java
+++ src/test/java/ar/edu/unq/desapp/grupoa/backenddesappapi/model/proyect/ProjectTest.java
@@ -1,6 +1,5 @@
 package ar.edu.unq.desapp.grupoa.backenddesappapi.model.proyect;
 
-import org.hibernate.tool.schema.extract.spi.ExtractionContext;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
