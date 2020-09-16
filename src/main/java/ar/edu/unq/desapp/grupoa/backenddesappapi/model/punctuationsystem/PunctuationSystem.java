ae8e11fb4e855a8c255c0eb2c59855a87650cb2f
AxelLopezGarabal <axel.lopez.garabal@gmail.com>
2020-09-16 09:41:36 -0300

    the punctuation system now can return the amount of points for one donation and donations

--- src/main/java/ar/edu/unq/desapp/grupoa/backenddesappapi/model/punctuationsystem/PunctuationSystem.java
+++ src/main/java/ar/edu/unq/desapp/grupoa/backenddesappapi/model/punctuationsystem/PunctuationSystem.java
@@ -2,14 +2,35 @@ package ar.edu.unq.desapp.grupoa.backenddesappapi.model.punctuationsystem;
 
 import ar.edu.unq.desapp.grupoa.backenddesappapi.model.interfaces.IRule;
 import ar.edu.unq.desapp.grupoa.backenddesappapi.model.proyect.Donation;
+import ar.edu.unq.desapp.grupoa.backenddesappapi.model.punctuationsystem.rule.TimesInTheMonth;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class PunctuationSystem {
+    private List<Product> products;
     private List<IRule> rules;
 
-    public PunctuationSystem(List<IRule> listOfRules) {
+    public PunctuationSystem(List<IRule> listOfRules, List<Product> productList) {
         this.rules = listOfRules;
+        this.products = productList;
+    }
+
+    /** METHODS RELATED TO THE PRODUCTS **/
+    public void addProduct(Product newProduct) {
+        this.products.add(newProduct);
+    }
+
+    public void deleteProduct(Product newProduct) {
+        this.products.remove(newProduct);
+    }
+
+    public List<Product> getProducts() {
+        return this.products;
+    }
+
+    public void setProductList(List<Product> newListOfProduct) {
+        this.products = newListOfProduct;
     }
 
     /** METHODS RELATED TO THE RULES **/
@@ -26,9 +47,21 @@ public class PunctuationSystem {
         this.rules.add(newRule);
     }
 
+    public void deleteRule(TimesInTheMonth newRule) {
+        this.rules.remove(newRule);
+    }
+
     /** METHODS RELATED TO THE DONATIONS **/
 
-    public Double calculatePointsForDonation(Donation aDonation) {
-        return 0.0;
+    public List<IRule> rulesApplicableToDonation(Donation aDonation) {
+        return this.rules.stream().filter(rule -> rule.isApplicable(aDonation)).collect(Collectors.toList());
+    }
+
+    public Double pointsGainForDonation(Donation aDonation) {
+        return this.rulesApplicableToDonation(aDonation).stream().mapToDouble(rule -> rule.pointsForDonation(aDonation)).sum();
+    }
+
+    public Double amountOfPointsForDonations(List<Donation> donations) {
+        return donations.stream().mapToDouble(donation -> this.pointsGainForDonation(donation)).sum();
     }
 }
--- src/test/java/ar/edu/unq/desapp/grupoa/backenddesappapi/model/punctuationsystem/PunctuationSystemTest.java
+++ src/test/java/ar/edu/unq/desapp/grupoa/backenddesappapi/model/punctuationsystem/PunctuationSystemTest.java
@@ -1,6 +1,7 @@
 package ar.edu.unq.desapp.grupoa.backenddesappapi.model.punctuationsystem;
 
 import ar.edu.unq.desapp.grupoa.backenddesappapi.model.interfaces.IRule;
+import ar.edu.unq.desapp.grupoa.backenddesappapi.model.proyect.Donation;
 import ar.edu.unq.desapp.grupoa.backenddesappapi.model.punctuationsystem.rule.InvertedCash;
 import ar.edu.unq.desapp.grupoa.backenddesappapi.model.punctuationsystem.rule.InvertedLocality;
 import ar.edu.unq.desapp.grupoa.backenddesappapi.model.punctuationsystem.rule.TimesInTheMonth;
@@ -12,16 +13,22 @@ import java.util.ArrayList;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 class PunctuationSystemTest {
 
     private PunctuationSystem punctuationSystem;
     private List<IRule> listOfRules = new ArrayList<>();
+    private List<Product> productList = new ArrayList<>();
+
+    private Donation aDonation = mock(Donation.class);
+    private Donation otherDonation = mock(Donation.class);
 
     @BeforeEach
     void setUp() {
-        punctuationSystem = new PunctuationSystem(listOfRules);
+        punctuationSystem = new PunctuationSystem(listOfRules, productList);
     }
 
     @AfterEach
@@ -66,4 +73,121 @@ class PunctuationSystemTest {
 
         assertEquals(punctuationSystem.getRules().get(0), newRule);
     }
+
+    @Test
+    public void test06WhenAPunctuationSystemReceivesTheMessageGetProductsRespondsWithItsListOfProducts(){
+        assertEquals(punctuationSystem.getProducts(), productList);
+    }
+
+    @Test
+    public void test07WhenAPunctuationSystemReceivesTheMessageAddProductsItAddTheProductToHisListOfProducts(){
+        Product newProduct = mock(Product.class);
+        punctuationSystem.addProduct(newProduct);
+
+        assertEquals(punctuationSystem.getProducts().get(0), newProduct);
+    }
+
+    @Test
+    public void test08WhenAPunctuationSystemReceivesTheMessageSetProductListHisListOfProductsChange(){
+        List<Product> newListOfProduct = new ArrayList<>();
+
+        punctuationSystem.setProductList(newListOfProduct);
+
+        assertEquals(punctuationSystem.getProducts(), newListOfProduct);
+    }
+
+    @Test
+    public void test09WhenAPunctuationSystemReceivesTheMessageDeleteRuleHeRemovesTheRulePassAsParameter(){
+        TimesInTheMonth newRule = mock(TimesInTheMonth.class);
+        punctuationSystem.addRule(newRule);
+
+        assertEquals(punctuationSystem.getRules().get(0), newRule);
+
+        punctuationSystem.deleteRule(newRule);
+
+        assertTrue(punctuationSystem.getRules().isEmpty());
+    }
+
+    @Test
+    public void test10WhenAPunctuationSystemReceivesTheMessageDeleteProductHeRemovesTheProductPassAsParameter(){
+        Product newProduct = mock(Product.class);
+        punctuationSystem.addProduct(newProduct);
+
+        assertEquals(punctuationSystem.getProducts().get(0), newProduct);
+
+        punctuationSystem.deleteProduct(newProduct);
+
+        assertTrue(punctuationSystem.getProducts().isEmpty());
+    }
+
+    @Test
+    public void test11WhenAPunctuationSystemReceivesTheMessageRulesApplicableToDonationHisRespondsWithAListOfRulesApplicableToTheDonation(){
+        InvertedCash forCash = mock(InvertedCash.class);
+        InvertedLocality forLocality = mock(InvertedLocality.class);
+
+        when(forCash.isApplicable(aDonation)).thenReturn(true);
+        when(forLocality.isApplicable(aDonation)).thenReturn(true);
+
+        punctuationSystem.addRule(forCash);
+        punctuationSystem.addRule(forLocality);
+
+        assertEquals(punctuationSystem.rulesApplicableToDonation(aDonation).size(), 2);
+    }
+
+    @Test
+    public void test12WhenAPunctuationSystemReceivesTheMessageRulesApplicableToDonationHisRespondsWithAListOfRulesApplicableToTheDonation(){
+        InvertedCash forCash = mock(InvertedCash.class);
+        InvertedLocality forLocality = mock(InvertedLocality.class);
+
+        when(forCash.isApplicable(aDonation)).thenReturn(false);
+        when(forLocality.isApplicable(aDonation)).thenReturn(false);
+
+        punctuationSystem.addRule(forCash);
+        punctuationSystem.addRule(forLocality);
+
+        assertTrue(punctuationSystem.rulesApplicableToDonation(aDonation).isEmpty());
+    }
+
+    @Test
+    public void test13WhenAPunctuationSystemReceivesTheMessagePointsGainForListOfDonationHisRespondsWithAAmountOfPointsGainedFrom(){
+        InvertedCash forCash = mock(InvertedCash.class);
+        InvertedLocality forLocality = mock(InvertedLocality.class);
+
+        when(forCash.pointsForDonation(aDonation)).thenReturn(2000.0);
+        when(forLocality.pointsForDonation(aDonation)).thenReturn(2000.0);
+
+        when(forCash.isApplicable(aDonation)).thenReturn(true);
+        when(forLocality.isApplicable(aDonation)).thenReturn(true);
+
+        punctuationSystem.addRule(forCash);
+        punctuationSystem.addRule(forLocality);
+
+        assertEquals(punctuationSystem.pointsGainForDonation(aDonation), 4000.0);
+    }
+
+    @Test
+    public void test14WhenAPunctuationSystemReceivesTheMessageamountOfPointsForDonationsHisRespondsIsTheAmountOfPoints(){
+        InvertedCash forCash = mock(InvertedCash.class);
+        InvertedLocality forLocality = mock(InvertedLocality.class);
+
+        when(forCash.pointsForDonation(aDonation)).thenReturn(2000.0);
+        when(forLocality.pointsForDonation(aDonation)).thenReturn(2000.0);
+
+        when(forCash.isApplicable(aDonation)).thenReturn(true);
+        when(forLocality.isApplicable(aDonation)).thenReturn(true);
+
+        when(forCash.isApplicable(otherDonation)).thenReturn(true);
+
+        when(forCash.pointsForDonation(otherDonation)).thenReturn(1000.0);
+
+        punctuationSystem.addRule(forCash);
+        punctuationSystem.addRule(forLocality);
+
+        List <Donation> donations = new ArrayList<>();
+        donations.add(aDonation);
+        donations.add(otherDonation);
+
+        assertEquals(5000.0, punctuationSystem.amountOfPointsForDonations(donations));
+    }
+
 }
