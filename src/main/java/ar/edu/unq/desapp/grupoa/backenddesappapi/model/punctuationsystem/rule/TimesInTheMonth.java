df151d1178b1cb5f25dda742140be4b6c1dec016
AxelLopezGarabal <axel.lopez.garabal@gmail.com>
2020-09-16 10:36:42 -0300

    the methos is applicable now requieres the user

--- src/main/java/ar/edu/unq/desapp/grupoa/backenddesappapi/model/punctuationsystem/rule/TimesInTheMonth.java
+++ src/main/java/ar/edu/unq/desapp/grupoa/backenddesappapi/model/punctuationsystem/rule/TimesInTheMonth.java
@@ -2,10 +2,11 @@ package ar.edu.unq.desapp.grupoa.backenddesappapi.model.punctuationsystem.rule;
 
 import ar.edu.unq.desapp.grupoa.backenddesappapi.model.interfaces.IRule;
 import ar.edu.unq.desapp.grupoa.backenddesappapi.model.proyect.Donation;
+import ar.edu.unq.desapp.grupoa.backenddesappapi.model.user.User;
 
 public class TimesInTheMonth implements IRule {
     @Override
-    public boolean isApplicable(Donation aDonation) {
+    public boolean isApplicable(Donation aDonation, User user) {
         return false;
     }
 
