d4baf8f758db65b9e6e086c7b31f2054b8fea765
AxelLopezGarabal <axel.lopez.garabal@gmail.com>
2020-09-16 09:32:34 -0300

    Rules now implements the methods of IRules

--- src/main/java/ar/edu/unq/desapp/grupoa/backenddesappapi/model/interfaces/IRule.java
+++ src/main/java/ar/edu/unq/desapp/grupoa/backenddesappapi/model/interfaces/IRule.java
@@ -1,4 +1,10 @@
 package ar.edu.unq.desapp.grupoa.backenddesappapi.model.interfaces;
 
+import ar.edu.unq.desapp.grupoa.backenddesappapi.model.proyect.Donation;
+
 public interface IRule {
+
+    boolean isApplicable(Donation aDonation);
+
+    Double pointsForDonation(Donation aDonation);
 }
--- src/main/java/ar/edu/unq/desapp/grupoa/backenddesappapi/model/punctuationsystem/rule/InvertedCash.java
+++ src/main/java/ar/edu/unq/desapp/grupoa/backenddesappapi/model/punctuationsystem/rule/InvertedCash.java
@@ -1,6 +1,16 @@
 package ar.edu.unq.desapp.grupoa.backenddesappapi.model.punctuationsystem.rule;
 
 import ar.edu.unq.desapp.grupoa.backenddesappapi.model.interfaces.IRule;
+import ar.edu.unq.desapp.grupoa.backenddesappapi.model.proyect.Donation;
 
 public class InvertedCash implements IRule {
+    @Override
+    public boolean isApplicable(Donation aDonation) {
+        return false;
+    }
+
+    @Override
+    public Double pointsForDonation(Donation aDonation) {
+        return null;
+    }
 }
