4300046acc3dd7cef64dce24689660c8a7ec46d6
AxelLopezGarabal <axel.lopez.garabal@gmail.com>
2020-09-18 19:10:34 -0300

    Locality id added

--- src/main/java/ar/edu/unq/desapp/grupoa/backenddesappapi/model/proyect/Locality.java
+++ src/main/java/ar/edu/unq/desapp/grupoa/backenddesappapi/model/proyect/Locality.java
@@ -1,12 +1,21 @@
 package ar.edu.unq.desapp.grupoa.backenddesappapi.model.proyect;
 
 public class Locality {
+    private Long localityId;
     private String name;
     private String province;
     private Integer population;
     private Double stateOfConnection;
 
     public Locality(String name, String province, Integer population, Double stateOfConnection) {
+        this.name = name;
+        this.province = province;
+        this.population = population;
+        this.stateOfConnection = stateOfConnection;
+    }
+
+    public Locality(Long id, String name, String province, Integer population, Double stateOfConnection) {
+        this.localityId = id;
         this.name = name;
         this.province = province;
         this.population = population;
@@ -39,5 +48,13 @@ public class Locality {
 
     public void setStateOfConnection(Double newStateOfConnection) {
         this.stateOfConnection = newStateOfConnection;
+    }
+
+    public Long getId() {
+        return this.localityId;
+    }
+
+    public void setId(long l) {
+        this.localityId = l;
     }
 }
