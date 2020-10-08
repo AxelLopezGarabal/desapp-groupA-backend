d461f0b40dcd919587e2498d458b241b481dba26
AxelLopezGarabal <axel.lopez.garabal@gmail.com>
2020-10-08 19:25:48 -0300

    mod of BackendDesappApiApplication to load data to de h2 database

--- src/main/java/ar/edu/unq/desapp/grupoa/backenddesappapi/BackendDesappApiApplication.java
+++ src/main/java/ar/edu/unq/desapp/grupoa/backenddesappapi/BackendDesappApiApplication.java
@@ -1,13 +1,30 @@
 package ar.edu.unq.desapp.grupoa.backenddesappapi;
 
+import ar.edu.unq.desapp.grupoa.backenddesappapi.dao.locality.LocalityDAO;
+import ar.edu.unq.desapp.grupoa.backenddesappapi.dao.project.ProjectDAO;
+import ar.edu.unq.desapp.grupoa.backenddesappapi.dao.punctuationsystem.PunctuationSystemDAO;
+import ar.edu.unq.desapp.grupoa.backenddesappapi.dao.user.UserDAO;
+import ar.edu.unq.desapp.grupoa.backenddesappapi.dao.wallet.WalletDAO;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.context.ConfigurableApplicationContext;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = SecurityAutoConfiguration.class)
 public class BackendDesappApiApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(BackendDesappApiApplication.class, args);
+	    ConfigurableApplicationContext configApp = SpringApplication.run(BackendDesappApiApplication.class, args);
+        LocalityDAO localityDAO = configApp.getBean(LocalityDAO.class);
+        ProjectDAO projectDAO = configApp.getBean(ProjectDAO.class);
+        UserDAO userDAO = configApp.getBean(UserDAO.class);
+        WalletDAO walletDAO = configApp.getBean(WalletDAO.class);
+        PunctuationSystemDAO systemDAO = configApp.getBean(PunctuationSystemDAO.class);
+
+        Loader loader = new Loader();
+        loader.addLocalitiesEntities(localityDAO);
+        loader.addProjectEntities(projectDAO);
+        loader.add(systemDAO, walletDAO, userDAO);
 	}
 
 }
