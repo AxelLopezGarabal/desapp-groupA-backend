4a377e8dc788fc9be0a495d797c92595b8603a23
AxelLopezGarabal <axel.lopez.garabal@gmail.com>
2020-10-24 10:55:41 -0300

    fix issue in the localityController

--- src/main/java/ar/edu/unq/desapp/grupoa/backenddesappapi/controllers/locality/LocalityController.java
+++ src/main/java/ar/edu/unq/desapp/grupoa/backenddesappapi/controllers/locality/LocalityController.java
@@ -8,7 +8,6 @@ import ar.edu.unq.desapp.grupoa.backenddesappapi.service.locality.LocalityServic
 import io.swagger.annotations.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
